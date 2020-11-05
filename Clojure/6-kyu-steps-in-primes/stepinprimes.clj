4a8eb71e0f46549b0d4708250676a229c14b819b
Selver Quinn <selver@localhost.localdomain>
2020-11-04 23:38:09 -0500

    Added updated stepinprimes and twosum given code

--- Clojure/6-kyu-steps-in-primes/stepinprimes.clj
+++ Clojure/6-kyu-steps-in-primes/stepinprimes.clj
@@ -1,5 +1,22 @@
-(ns stepinprimes.core)
+(ns stepinprimes)
 
 (defn step [g m n]
-  ; your code
+  (def res nil)
+  (def rng (filter prime? (range m n)))
+  (loop [n1 (next rng)]
+    (println (first n1))
+    (def res (some #{(- (first n1) g)} rng))
+    (if (and (second n1) (not res)) (recur (next n1))))
+  (vector res (+ res g))
 )
+
+(defn prime? [x]
+  (def res false)
+  (if (> x 2)
+    (loop [n 2]
+      (if (<= n (Math/sqrt x))
+             (if (not= (mod x n) 0)
+               (recur (inc n)))
+             (def res true))))
+  res
+) 
---
+++ Clojure/6-kyu-two-sum/twosum.clj
@@ -0,0 +1,4 @@
+(ns twosum)
+(defn twosum [numbers target]
+  ;...
+)
