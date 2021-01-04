292f7ff3a885c9edf876c326b9eec0c4f20eccd6
Selver <koylastrasky@gmail.com>
2021-01-04 06:34:42 -0500

    Removed extraneous k in loop

--- Clojure/5-kyu-josephus-survivor/josephussurvivor.clj
+++ Clojure/5-kyu-josephus-survivor/josephussurvivor.clj
@@ -1,6 +1,6 @@
 (ns josephusurvivor.core)
 
 (defn josephus-survivor [n k]
-  (loop [n n, k k, pos #(inc (mod (dec %) n))]
+  (loop [n n, pos #(inc (mod (dec %) n))]
     (if (= 1 n) (pos 1)
-      (recur (dec n) k (comp pos #(inc (mod (+ (dec k) %) n)))))))
+      (recur (dec n) (comp pos #(inc (mod (+ (dec k) %) n)))))))
