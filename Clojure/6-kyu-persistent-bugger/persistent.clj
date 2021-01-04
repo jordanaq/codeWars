54ceb2f71c7ed22e98039bead6351b8b0657c51f
SelverKoyla <koylastrasky@gmail.com>
2021-01-04 04:40:16 -0500

    Added code to brute force find it (Going to look for obvious pattern)

--- Clojure/6-kyu-persistent-bugger/persistent.clj
+++ Clojure/6-kyu-persistent-bugger/persistent.clj
@@ -1,5 +1,6 @@
 (ns persistent.core)
 
 (defn persistence [n]
-  0)
-  
+  (loop [n n, iter 0]
+    (if (< n 10) iter
+      (recur (reduce #(+ (- (int %1) 48) (- (int %2) 48)) (str n)) (inc iter)))))
