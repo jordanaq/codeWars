20579e3286ee6c9a44a7c6d0c72cf3b0779a86e6
Selver <koylastrasky@gmail.com>
2021-01-18 22:34:43 -0500

    Solved the kata
    I originally solved it dumb-like by using a case statement and a recur in each case. I didn't like this so I rewrote it as seen

--- Clojure/5-kyu-greed-is-good/kata.clj
+++ Clojure/5-kyu-greed-is-good/kata.clj
@@ -1,5 +1,9 @@
 (ns kata)
 
 (defn score [dice]
-  ; TODO
-  )
+  (let [[[_ x] [_ y] & z] (map #(vector % (count (filter #{%} dice))) [1 5 2 3 4 6])]
+    (+ (if (>= x 3) 1000 0)
+       (* (mod x 3) 100)
+       (if (>= y 3) 500 0)
+       (* (mod y 3) 50)
+       (apply + (map #(if (>= (second %) 3) (* (first %) 100) 0) z)))))
