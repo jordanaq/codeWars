a31abad04fb5f650577abb0320adc3d6ef2ecc56
Selver <koylastrasky@gmail.com>
2021-01-19 02:04:34 -0500

    Update mixin.clj
    Wrote the code as per the instructions but there's a hidden requirement dealing with chars that appear an equal amount of times in each string. I need to put those ones behind other chars that appear equally frequently

--- Clojure/4-kyu-strings-mix/mixin.clj
+++ Clojure/4-kyu-strings-mix/mixin.clj
@@ -2,4 +2,16 @@
 (require '[clojure.string :as str])
 
 (defn mix [s1 s2]
-  ; your code)
+  (let [lowers (map char (range 97 123))
+        nums (fn [s] (into (sorted-map) (sort-by second > (filterv #(> (second %) 1) (map #(vector % (count (filter #{%} s))) lowers)))))
+        a (nums s1)
+        b (nums s2)
+        c (merge-with #(if (> %1 %2) %1 %2) a b)]
+    (do (def res "")
+      (doseq [x c]
+        (if (= (get a (first x)) (get b (first x)))
+          (def res (str res \= \: (apply str (repeat (second x) (first x))) \/))
+          (if (= (get a (first x)) (second x))
+            (def res (str res \1 \: (apply str (repeat (second x) (first x))) \/))
+            (def res (str res \2 \: (apply str (repeat (second x) (first x))) \/)))))
+      (if (empty? res) "" (subs res 0 (dec (count res)))))))
