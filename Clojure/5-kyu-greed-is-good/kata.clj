f80741e92f9e3d2798aee6eb271752bc4dd890a5
Selver <koylastrasky@gmail.com>
2021-01-18 22:50:26 -0500

    Update kata.clj
    Replaced the weird if statements with quot (truncation division) after learning of quot from the other solutions

--- Clojure/5-kyu-greed-is-good/kata.clj
+++ Clojure/5-kyu-greed-is-good/kata.clj
@@ -2,8 +2,8 @@
 
 (defn score [dice]
   (let [[[_ x] [_ y] & z] (map #(vector % (count (filter #{%} dice))) [1 5 2 3 4 6])]
-    (+ (if (>= x 3) 1000 0)
+    (+ (* (quot x 3) 1000)
        (* (mod x 3) 100)
-       (if (>= y 3) 500 0)
+       (* (quot y 3) 500)
        (* (mod y 3) 50)
        (apply + (map #(if (>= (second %) 3) (* (first %) 100) 0) z)))))
