042bab6bd42040ad4534567bba5bda4cb32f34cb
Selver <koylastrasky@gmail.com>
2021-01-20 22:36:49 -0500

    Update mixin.clj
    made a custom comparator to sort a vec of my final substrings by count first then by the ascii value of the first char. The letters themselves were in alphabetical order from when I defined c

--- Clojure/4-kyu-strings-mix/mixin.clj
+++ Clojure/4-kyu-strings-mix/mixin.clj
@@ -3,15 +3,16 @@
 
 (defn mix [s1 s2]
   (let [lowers (map char (range 97 123))
-        nums (fn [s] (into (sorted-map) (sort-by second > (filterv #(> (second %) 1) (map #(vector % (count (filter #{%} s))) lowers)))))
+        nums (fn [s] (into (hash-map) (filterv #(> (second %) 1) (map #(vector % (count (filter #{%} s))) lowers))))
         a (nums s1)
         b (nums s2)
-        c (merge-with #(if (> %1 %2) %1 %2) a b)]
-    (do (def res "")
+        c (sort (merge-with #(if (> %1 %2) %1 %2) a b))]
+    (do 
+      (def res [])
       (doseq [x c]
-        (if (= (get a (first x)) (get b (first x)))
-          (def res (str res \= \: (apply str (repeat (second x) (first x))) \/))
-          (if (= (get a (first x)) (second x))
-            (def res (str res \1 \: (apply str (repeat (second x) (first x))) \/))
-            (def res (str res \2 \: (apply str (repeat (second x) (first x))) \/)))))
-      (if (empty? res) "" (subs res 0 (dec (count res)))))))
+          (if (= (get a (first x)) (get b (first x)))
+            (def res (conj res (str \= \: (apply str (repeat (second x) (first x))))))
+            (if (= (get a (first x)) (second x))
+              (def res (conj res (str \1 \: (apply str (repeat (second x) (first x))))))
+              (def res (conj res (str \2 \: (apply str (repeat (second x) (first x)))))))))
+      (if (empty? res) "" (str/join \/ (sort-by #(+ (* (count %) -1000) (int (first %)))  res))))))
