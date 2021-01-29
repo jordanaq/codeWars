aa71a2e16dc7a5a29aeda127443efea57e11c13d
Jordan Quinn <koylastrasky@gmail.com>
2021-01-29 14:38:25 -0500

    Turned it into a one-liner
    Used the or operator to return the list when logically true and the string when the list is logically false

--- Python/7-kyu-find-the-divisors/divisors.py
+++ Python/7-kyu-find-the-divisors/divisors.py
@@ -1,4 +1,2 @@
 def divisors(integer):
-    res = [i for i in range(2, integer // 2 + 1) if integer % i == 0]
-    
-    return "%d is prime" % integer if res == [] else res
+    return [i for i in range(2, integer // 2 + 1) if integer % i == 0] or "%d is prime" % integer
