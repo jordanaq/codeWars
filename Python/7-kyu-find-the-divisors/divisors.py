7876a5b91a8a97713e72bccc641bbb9855d2f77a
Jordan Quinn <koylastrasky@gmail.com>
2021-01-29 14:30:06 -0500

    Wrote the solution

--- Python/7-kyu-find-the-divisors/divisors.py
+++ Python/7-kyu-find-the-divisors/divisors.py
@@ -1,2 +1,11 @@
 def divisors(integer):
-    pass
+    res = []
+    
+    for i in range(2, integer // 2 + 1):
+        if integer % i == 0:
+            res.append(i)
+    
+    if res == []:
+        return str(integer) + " is prime"
+    else:
+        return res
