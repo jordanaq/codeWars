63163f4492c64536fbce807bfa84d561942cac75
Jordan Quinn <koylastrasky@gmail.com>
2021-02-03 13:34:28 -0500

    Added description of the function to src file and readme

--- Clojure/5-kyu-largest-difference-in-increasing-indexes/src/largest_difference/core.clj
+++ Clojure/5-kyu-largest-difference-in-increasing-indexes/src/largest_difference/core.clj
@@ -2,6 +2,9 @@
 
 
 (defn largestDifference [data]
+  "Finds the largest difference in indexes between in a collection
+  given that the value in the larger index is greater than or equal
+  to the value in the smaller index."
   (apply max (for [start (range (count data))
                    end (reverse (range (count data)))
                    :let [diff (- end start)]
