a0cb00f01f763ffa127ce2202a63e3018a9611da
Jordan Quinn <koylastrasky@gmail.com>
2021-02-02 20:27:06 -0500

    Added testing code to main

--- C++/5-kyu-decimal-to-factorial-and-back/Dec2Fact.cpp
+++ C++/5-kyu-decimal-to-factorial-and-back/Dec2Fact.cpp
@@ -3,6 +3,11 @@
 class Dec2Fact
 {
 public:
-    static std::string dec2FactString(long long nb);
-    static long long factString2Dec(const std::string &str);
+    static std::string dec2FactString(long long nb) {
+        return "wrong";
+    };
+
+    static long long factString2Dec(const std::string &str) {
+        return 0;
+    };
 };
--- C++/5-kyu-decimal-to-factorial-and-back/main.cpp
+++ C++/5-kyu-decimal-to-factorial-and-back/main.cpp
@@ -2,6 +2,17 @@
 #include "Dec2Fact.cpp"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    //  dec2FactString tests
+    std::cout << "dec2FactString(2982) = " << Dec2Fact::dec2FactString(2982) << " and " <<
+            ((Dec2Fact::dec2FactString(2982) == "4041000") ? "IS" : "ISN'T") << " 4041000." << std::endl;
+    std::cout << "dec2FactString(463) = " << Dec2Fact::dec2FactString(463) << " and " <<
+            ((Dec2Fact::dec2FactString(463) == "341010") ? "IS" : "ISN'T") << " 341010." << std::endl;
+
+    //  factString2Dec tests
+    std::cout << "factString2Dec(\"4041000\") = " << Dec2Fact::factString2Dec("4041000") << " and " <<
+              ((Dec2Fact::factString2Dec("4041000") == 2982) ? "IS" : "ISN'T") << " 2982." << std::endl;
+    std::cout << "factString2Dec(\"341010\") = " << Dec2Fact::factString2Dec("341010") << " and " <<
+              ((Dec2Fact::factString2Dec("4041000") == 463) ? "IS" : "ISN'T") << " 463." << std::endl;
+
     return 0;
 }
