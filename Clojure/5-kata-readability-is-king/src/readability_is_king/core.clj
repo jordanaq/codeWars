3df6354015b8f360c7702c776f833d9866635d92
Jordan Quinn <koylastrasky@gmail.com>
2021-02-03 19:20:18 -0500

    Finished the readability kata
    My first actual use of regex!

--- Clojure/5-kata-readability-is-king/src/readability_is_king/core.clj
+++ Clojure/5-kata-readability-is-king/src/readability_is_king/core.clj
@@ -1,5 +1,8 @@
-(ns readability-is-king.core)
+(ns readability-is-king.core
+  (:require [clojure.string :as s]))
 
 (defn flesch-kincaid [text]
-  ;; TODO: succeed!
-  )
+  (let [wordNum (count (s/split text #"[ ]"))
+        sentNum (count (s/split text #"[.!?]"))
+        sylNum (count (filter #(not (s/blank? %)) (s/split (s/lower-case text) #"[bcdfghjklmnpqrstvwxz -A]")))]
+    (+ (* 0.39 (/ wordNum sentNum)) (* 11.8 (/ sylNum wordNum)) -15.59)))
