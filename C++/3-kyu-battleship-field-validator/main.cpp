40d58e5508e5e0e20c7f3973f5dc4b745f33c474
tsiru <quinn.jordan@protonmail.ch>
2021-06-20 02:17:04 -0400

    Commented code and removed redundancies

--- C++/3-kyu-battleship-field-validator/main.cpp
+++ C++/3-kyu-battleship-field-validator/main.cpp
@@ -1,35 +1,53 @@
 #include <iostream>
 
 #include <vector>
-#include <array>
+
+// getPos needed in struct definition
+int getPos(std::vector<std::vector<int>> &, int, int);
+
+
 
 struct neighbors {
-    // UpLeft,
-    int flags : 6;
+    // flags: diagonalNeighbor, neighborUp, neighborDown, neighborLeft, neighborRight
+    int flags : 5;
 
-    bool operator==(const neighbors o) const {
+    // Constructors, one empty constructor and one based on a vector and coords
+    // neighbors is trivial copy constructable
+    neighbors() =default;
+    neighbors(const neighbors & o) =default;
+    neighbors(std::vector<std::vector<int>> &field, int x, int y) : flags {
+                    // Cardinal positions
+        /*up:*/     (getPos(field, x, y-1)<<3) +
+        /*down:*/   (getPos(field, x, y+1)<<2) +
+        /*left:*/   (getPos(field, x-1, y)<<1) +
+        /*right:*/  (getPos(field, x+1, y)<<0) +
+
+                    // Diagonal positions. All have the same effect and share a flag
+        /*topL:*/   ((getPos(field, x-1, y-1)|
+        /*topR:*/     getPos(field, x+1, y-1)|
+        /*botL:*/     getPos(field, x-1, y+1)|
+        /*botR:*/     getPos(field, x+1, y+1))<<4)
+        } { }
+
+
+    // Checks if two neighbors' flags are equal or if they are in a straight line
+    bool operator==(const neighbors &o) const {
         return flags == o.flags || (flags == 0b1100 && o.flags == 0b0100) || (flags == 0b0011 && o.flags == 0b0001);
     }
 
-    /*bool validNeighbors() const {
-        return (flags == 0     ) ||
-               (flags == 0b1000) ||
-               (flags == 0b0100) ||
-               (flags == 0b0010) ||
-               (flags == 0b0001) ||
-               (flags == 0b1100) ||
-               (flags == 0b0011);
-    }*/
+    // Explicitly declared as it is required
+    bool operator!=(const neighbors &o) const { return !(*this==o); };
 
-    explicit operator bool() const { return (flags == 0     ) ||
-                                            (flags == 0b1000) ||
-                                            (flags == 0b0100) ||
-                                            (flags == 0b0010) ||
-                                            (flags == 0b0001) ||
-                                            (flags == 0b1100) ||
-                                            (flags == 0b0011);      }
+    // To be used in if statements
+    // Returns true when there are no diagonal neighbors
+    explicit operator bool() const { return flags != 0b10000; }
+
 };
 
+
+
+// returns the value in field[y][x] or 0 if out of range
+// std::optional<int> is a potential replacement with better code but is slightly less performant
 int getPos(std::vector<std::vector<int>> &field, int x, int y) {
     try {
         return field.at(y).at(x);
@@ -38,67 +56,68 @@ int getPos(std::vector<std::vector<int>> &field, int x, int y) {
     }
 }
 
-neighbors getNeighbors(std::vector<std::vector<int>> &field, int x, int y) {
-    return {
-                // Cardinals
-    /*up:*/     (getPos(field, x, y-1)<<3) +
-    /*down:*/   (getPos(field, x, y+1)<<2) +
-    /*left:*/   (getPos(field, x-1, y)<<1) +
-    /*right:*/  (getPos(field, x+1, y)<<0) +
-
-                // Diagonals
-    /*topL:*/   ((getPos(field, x-1, y-1)|
-    /*topR:*/     getPos(field, x+1, y-1)|
-    /*botL:*/     getPos(field, x-1, y+1)|
-    /*botR:*/     getPos(field, x+1, y+1))<<4)
-    };
-}
 
 
-int validShip (std::vector<std::vector<int>> &field, int x, int y){//}), neighbors prev) {
+// Pure recursive function. Returns the length of a ship
+__attribute((pure)) int shipLen (std::vector<std::vector<int>> &field, int x, int y) {//}), neighbors prev) {
+    // If the function reached the end of the board or a empty space
     if (y < 0 || y > field.size() || x < 0 || x > field[y].size() || !field[y][x]) return 0;
 
-    neighbors curr = getNeighbors(field, x, y);
-    //if (curr == (neighbors){ 0 }) return 1;
+    // Generate flags for current position
+    neighbors curr{ field, x, y };
+
 
     switch (curr.flags) {
+        // Diagonal or edge case
         default:
-
-        case 0b10000:
             return INT32_MIN;
-        case 0: case 0b1000:
+
+        // case when current position is the end of a ship
+        case 0: case 0b1000: case 0b0010:
             return 1;
+
+        // case when current position is an up/down ship
         case 0b0100: case 0b1100:
-            return 1 + validShip(field, x, y+1);//), curr);
-        case 0b0010:
-            return 1;
+            return 1 + shipLen(field, x, y+1);//), curr);
+
+        // case when current position is a left/right ship
         case 0b0001: case 0b0011:
-            return 1 + validShip(field, x+1, y);//, curr);
+            return 1 + shipLen(field, x+1, y);//, curr);
     }
 }
 
 
 
+// Determines if field is a legal battlefield board
 bool validate_battlefield(std::vector< std::vector<int> > field) {
+    // Empty vector to hold the count of the four possible ship lengths
     std::vector<char> ships { 0, 0, 0, 0 };
 
-    auto curr = getNeighbors(field, -1, -1);
+    // Two for loops iterate over every possible x and y value combination
     for (int y = 0; y < field.size(); y++) {
         for (int x = 0; x < field[y].size(); x++) {
 
+            // Checks if current position is a 1
             if(field[y][x]) {
-                if ((bool) (curr = getNeighbors(field, x, y))) {
-                    std::cout << '(' << x << ',' << -y << ')' << "\tf: " << curr.flags << "\tl: " << validShip(field, x, y)<<std::endl;//, {~0}) << std::endl;
+
+                // Creates a neighbors struct and checks for diagonal neighbor
+                if (neighbors curr { field, x, y }; (bool) curr) {
+
+                    // Checks if the current position has already been processed
                     if (auto f = curr.flags; f==0b1000 || f==0b0010 || f==0b1100 || f==0b0011) continue;
-                    else if (auto len = validShip(field, x, y); f == 0 || (len <= 4 && len > 0)) {
-                        if ((++ships[len - 1]) > (5 - len)) return false;
-                    }
+
+                    // Increments ships[len-1] when len=(0,4] with early return
+                    else if (auto len = shipLen(field, x, y); len <= 0 || len > 4) return false;
+                    else ships[len-1]++;
+
+                // returns false if a diagonal neighbor exists
                 } else return false;
             }
         }
     }
 
-    std::cout << ships[0] << " " << ships[1] << " " << ships[2] << " " << ships[3] << std::endl ;
+
+    // Returns true only if it hasn't returned by this point and if all ships exist
     return ships[0]==4 && ships[1]==3 && ships[2]==2 && ships[3]==1;
 }
 
