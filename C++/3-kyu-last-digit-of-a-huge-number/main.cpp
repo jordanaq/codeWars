23aea81e58a5c14de3d02ce8a6b19a5a08fe67fa
tsiru <quinn.jordan@protonmail.ch>
2021-06-21 22:45:12 -0400

    -

--- C++/3-kyu-last-digit-of-a-huge-number/main.cpp
+++ C++/3-kyu-last-digit-of-a-huge-number/main.cpp
@@ -1,6 +1,7 @@
 #include <list>
 #include <cmath>
 #include <iostream>
+#include <iomanip>
 
 //using namespace std;
 
@@ -13,34 +14,41 @@ int last_digit(const std::string &str1, const std::string &str2) {
             *--str2.end() - '0' :
             std::stoi(str2.substr(str2.size()-2, 2)) };
 
-    std::cout<< (long long) std::pow(base, 4 + exp%4) % 10LL;
+    return (long long) std::pow(base, 4 + exp%4) % 10LL;
 }
+
+//inline auto iPow = [](long long i, long long g) -> long long { return (!i && !g) ? 1 : std::pow(i, g); };
+typedef unsigned long long ll;
 
 int last_digit(std::list<int> array) {
     if (array.empty()) return 1;
+    else if (auto a = array.begin(); *a%10==1 || *a%10==5 || *a%10==6) return *a%10;
 
-    int last_digit = *std::end(array);
-    for (auto i = --std::end(array); i != std::begin(array); i++) {
-        if (!last_digit) last_digit = *i;
+    auto m = std::modulus<ll>();
 
-        last_digit = pow(*i,last_digit%4);
+    ll exp { (ll) array.back() };
+    for (auto i = ----array.end(); i != array.begin(); i--) {std::cout<<"exp: "<<exp<<"\ti: "<<*i<<"\t";
+        if (exp)
+            exp = (exp<=2)&&*i%4!=2 ? m(*i,4)*std::pow(m(*i,4), (3+exp)%4) : 0;
+        else exp = 1;
     }
 
-    return last_digit % 10;
+    std::cout << "\texp:" << exp << '\t';
+    return array.front()||exp ? m(std::pow(m(array.front(),10), 4+m(exp,4)),10) : 1;
 }
 
 int main() {
     std::cout <<
-              "last_digit({})" << '\t' << (last_digit({})==1) << std::endl <<
-              "last_digit({0, 0})" << '\t' << (last_digit({0, 0})==1) << std::endl <<
-              "last_digit({0,0,0}) "<< '\t' << (last_digit({0,0, 0})==0) << std::endl <<
-              "last_digit({1,2})" << '\t' << (last_digit({01, 2})==1) << std::endl <<
-              "last_digit({3,4,5})" << '\t' << (last_digit({3, 4, 5})==1) << std::endl <<
-              "last_digit({4,3,6})" << '\t' << (last_digit({4,3,6})==4) << std::endl <<
-              "last_digit({7,6,21})"<< '\t' << (last_digit({7,6,21})==1) << std:: endl <<
-              "last_digit({12,30,21})"<< '\t' << (last_digit({12,30,21})==6) << std::endl <<
-              "last_digit({2,2,2,0})"<< '\t' << (last_digit({2,2,2,0})==4) << std:: endl <<
-              "last_digit({937640,767456,981242})"<< '\t' << (last_digit({7,6,21})==0) << std::endl<<
-              "last_digit({123232,694022,140249})"<< '\t' << (last_digit({7,6,21})==6) << std::endl<<
-              "last_digit({499942,898102,846073})"<< '\t' << (last_digit({7,6,21})==6) << std::endl;
+              "last_digit({})" << "\nres:" << (last_digit({})==1) << '.' << std::endl << std::endl <<
+              "last_digit({0, 0})" << "\nres:" << (last_digit({0, 0})==1) << '.' << std::endl << std::endl <<
+              "last_digit({0,0,0}) "<< "\nres:" << (last_digit({0,0,0})==0) << '.' << std::endl << std::endl <<
+              "last_digit({1,2})" << "\nres:" << (last_digit({01, 2})==1) << '.' << std::endl << std::endl <<
+              "last_digit({3,4,5})" << "\nres:" << (last_digit({3, 4, 5})==1)<< '.'  << std::endl << std::endl <<
+              "last_digit({4,3,6})" << "\nres:" << (last_digit({4,3,6})==4) << '.' << std::endl << std::endl <<
+              "last_digit({7,6,21})"<< "\nres:" << (last_digit({7,6,21})==1) << '.' << std:: endl << std::endl <<
+              "last_digit({12,30,21})"<< "\nres:" << (last_digit({12,30,21})==6)<< '.'  << std::endl << std::endl <<
+              "last_digit({2,2,2,0})"<< "\nres:" << (last_digit({2,2,2,0})==4) << '.' << std:: endl << std::endl <<
+              "last_digit({937640,767456,981242})"<< "\nres:" << (last_digit({7,6,21})==0)<< '.'  << std::endl<< std::endl <<
+              "last_digit({123232,694022,140249})"<< "\nres:" << (last_digit({7,6,21})==6) << '.' << std::endl<< std::endl <<
+              "last_digit({499942,898102,846073})"<< "\nres:" << (last_digit({7,6,21})==6)<< '.'  << std::endl;
 }
