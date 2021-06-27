9b0c6d96388b2c11bae1fc0c8c72cf3cf2fc73ef
tsiru <quinn.jordan@protonmail.ch>
2021-06-26 22:07:07 -0400

    added given code for this kata

---
+++ C++/3-kyu-Rail-Fence-Cipher-Encoding-and-Decoding/main.cpp
@@ -0,0 +1,10 @@
+#include <string>
+
+std::string encode_rail_fence_cipher(const std::string &str, int n) {
+  return "";
+}
+
+std::string decode_rail_fence_cipher(const std::string &str, int n) {
+
+  return "";
+}
