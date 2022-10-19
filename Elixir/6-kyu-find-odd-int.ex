c70fea892558cc1210e76722971b088e589c0af1
Jordan Quinn <jaq1856@g.rit.edu>
2022-10-18 20:50:46 -0400

    Changed formatting slightly to reduce clutter

--- Elixir/6-kyu-find-odd-int.ex
+++ Elixir/6-kyu-find-odd-int.ex
@@ -1,6 +1,7 @@
 defmodule FindOdd do
   def find(list) do
-    Enum.reduce( list, %{}, &Map.update(&2, &1, 1, fn i -> i+1 end) )
+    list
+    |> Enum.reduce( %{}, &Map.update(&2, &1, 1, fn i -> i+1 end) )
     |> Map.to_list
     |> Enum.find_value( fn {k, v} -> if( rem(v, 2) == 1, do: k) end )
   end
