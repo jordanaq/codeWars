6df103ccb86b668fa6e39f58b643721bc3add031
Jordan Quinn <koylastrasky@gmail.com>
2022-11-01 18:54:48 -0400

    Commented code

--- Elixir/5-kyu-the-hashtag-generator.ex
+++ Elixir/5-kyu-the-hashtag-generator.ex
@@ -1,8 +1,14 @@
 defmodule Hashtag do
+  # Catch empty string
   defp return_str(""), do: false
+  
+  # Catch when there are more than 140 characters, assumed single byte chars
   defp return_str(str) when byte_size(str) >= 140, do: false
+  
+  # Retrun str prepended with a hashtag otherwise
   defp return_str(str), do: "#" <> str
   
+  # Takes an input string and generates a proper capitalized hashtag
   def generate(input) do
     input
     |> String.split(" ", trim: true)
