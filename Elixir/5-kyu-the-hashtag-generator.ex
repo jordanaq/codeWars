371322a1ddd642e958916bbb685f60d90de12dd8
Jordan Quinn <koylastrasky@gmail.com>
2022-11-01 18:47:00 -0400

    First iteration of hashtag generator algorithm

--- Elixir/5-kyu-the-hashtag-generator.ex
+++ Elixir/5-kyu-the-hashtag-generator.ex
@@ -1,1 +1,11 @@
-
+defmodule Hashtag do
+  defp valid_len(str), do: String.length(str) < 140
+  defp gen(""), do: false
+  defp gen(input) do
+    input
+    |> String.split
+    |> Enum.map_join(&String.capitalize/1)
+    |> (&(if valid_len(&1), do: "#" <> &1, else: false)).()
+  end
+  def generate(input), do: gen(String.trim input)
+end
